0e32faf32cb76cadef5d39802c29cecbcb81d4f0
oguzkaanmertaslan <kmertaslan66@gmail.com>
2022-02-10 23:02:40 +0300

    movies grid css updated

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,10 +5,10 @@ const MovieList = (props) => {
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div className="image-container d-flex justify-content-start m-3">
+        <div className="active image-container d-flex justify-content-start m-3">
           <img src={movie.Poster} alt="movie" />
           <div
-            onClick={()=>props.handleFavoritesClick(movie)}
+            onClick={() => props.handleFavoritesClick(movie)}
             className="overlay d-flex- align-items-center justify-content-center"
           >
             <FavouriteComponents />
